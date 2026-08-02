ff851850c6f8943c2fb9faac2c2b430212439752
OhadRaza01 <ohadraza345@gmail.com>
2026-08-03 03:19:43 +0500

    Add routes for update avatar and cover image controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser , updateAccountDetails } from "../controllers/user.controller.js";
+import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser , updateAccountDetails, updateAvatar, updateCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import jwtVerify from "../middlewares/auth.middleware.js";
 
@@ -30,5 +30,17 @@ router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(jwtVerify , changeCurrentPassword)
 
 router.route("/update-account-info").post(jwtVerify , updateAccountDetails)
+
+router.route("/update-avatar").post(
+    jwtVerify,
+    upload.single("avatar"),
+    updateAvatar
+)
+
+router.route("/update-cover-image").post(
+    jwtVerify,
+    upload.single("coverImage"),
+    updateCoverImage
+)
 
 export default router
